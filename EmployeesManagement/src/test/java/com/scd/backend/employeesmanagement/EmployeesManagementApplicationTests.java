4dd3bac33c9c83dc1e7268751db0f27f77777dfa
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-10-24 18:33:01 +0300

    Updates

---
+++ EmployeesManagement/src/test/java/com/scd/backend/employeesmanagement/EmployeesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scd.backend.employeesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
